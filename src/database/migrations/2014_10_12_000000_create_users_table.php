a0ae55ed8513b7ca5692b791853681bf95f2f6b2
unknown <bocalist@gmail.com>
2020-05-08 13:32:23 +0900

    유저 테이블 수정(생성)

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 20);
+            $table->string('nickname', 30);
+            $table->string('password');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('phone', 20)->unique();
+            $table->enum('gender', ['m', 'f'])->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
