9e6df1ef0e4daf249ec936d59a7dfce70092e2a4
unknown <bocalist@gmail.com>
2020-05-08 15:29:43 +0900

    api 라우팅 추가

--- src/routes/api.php
+++ src/routes/api.php
@@ -14,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// 인증이 필요한 API endpoint (Sanctum 이용)
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/logout', 'Auth\LoginController@logout'); // 로그아웃
+    Route::get('/user/{id}', 'UserController@getUser'); // 단일 회원 상세 정보 조회
+    Route::get('/user/{id}/orders', 'UserController@getOrders'); // 단일 회원의 주문목록 조회
+    Route::post('/users', 'UserController@getUsers'); // 여러 회원 목록 조회
+});
+
+// 인증이 불필요한 API endpoint
+Route::group(['middleware' => ['guest:api']], function () {
+    Route::post('/register', 'Auth\RegisterController@register'); // 회원가입
+    Route::post('/login', 'Auth\LoginController@login'); // 로그인
 });
